4cc86a52c041d9f2fc015f165ba2ecb9473e1aa3
Carlos Daniel <killer.dk20@gmail.com>
2021-02-27 14:07:07 -0300

    feat: tratando erros

--- lib/danielspay/numbers.ex
+++ lib/danielspay/numbers.ex
@@ -1,14 +1,9 @@
 defmodule Danielspay.Numbers do
   def sum_from_file(filename) do
-    # Obervando do lado direito temos uma função que retorna uma *tupla* com 2 valores
-    # e do lado esquerdo criamos uma *tupla* com 2 valores, se a *tupla* do do lado
-    # direito retornar o mesmo padrão da *tupla* do lado esquerdo teremos um "MATCH" (os dois se coincidem)
-    # e isso será verdadeiro, logo a variável *file* irá receber o valor de retorno da função do lado direito
-
-    # Tupla = Coleção de Dados
-    # File = variavél
-    # LADO ESQUERO = LADO DIREITO
-    {:ok, file} = File.read("#{filename}.csv")
-    file
+    file = File.read("#{filename}.csv")
+    handle_file(file)
   end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error, "Invalid File"}
 end
