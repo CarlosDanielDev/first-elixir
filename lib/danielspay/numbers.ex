b92dd93921add1a594e49b45014051fc6c562358
Carlos Daniel <killer.dk20@gmail.com>
2021-02-27 14:24:41 -0300

    feat: sum numbers from file

--- lib/danielspay/numbers.ex
+++ lib/danielspay/numbers.ex
@@ -8,6 +8,11 @@ defmodule Danielspay.Numbers do
     |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result
+    |> String.split(",")
+    |> Enum.map(fn number -> String.to_integer(number) end )
+    |> Enum.sum()
+  end
   defp handle_file({:error, _reason}), do: {:error, "Invalid File"}
 end
