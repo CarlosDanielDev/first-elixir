23d0932801e39f94ff40ad08fe2a06490c915693
Carlos Daniel <killer.dk20@gmail.com>
2021-02-27 14:15:34 -0300

    explain: pipe operator

--- lib/danielspay/numbers.ex
+++ lib/danielspay/numbers.ex
@@ -1,8 +1,11 @@
 defmodule Danielspay.Numbers do
   def sum_from_file(filename) do
+    # Pipe Operator, composto por um Pipe | e um sinal de maior que >
+    # Ele pega a primeira expressão antes dele, nesse caso o "#{filename}.csv"
+    # e passa de forma impicita como primeiro argumento para as funções seguintes
     "#{filename}.csv"
-    |>File.read()
-    |>handle_file()
+    |> File.read()
+    |> handle_file()
   end
 
   defp handle_file({:ok, file}), do: file
