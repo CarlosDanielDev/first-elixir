c043d6f2e2f62973f11ff7743af332215010ae60
Carlos Daniel <killer.dk20@gmail.com>
2021-02-27 13:38:54 -0300

    explain: Pattern Match

--- lib/danielspay/numbers.ex
+++ lib/danielspay/numbers.ex
@@ -1,5 +1,9 @@
 defmodule Danielspay.Numbers do
   def sum_from_file(filename) do
+    # do lado direito temo uma função que retorna uma tupla com 2 valores
+    # e do lado direito criamo uma tupla com 2 valores, se a tuplado do lado
+    # direito retornar algo como a tupla do lado esquerdo teremos um "MATCH"
+    # e isso será verdadeiro, logo file irá receber o valor de retorno da função da direita
     {:ok, file} = File.read("#{filename}.csv")
     file
   end
