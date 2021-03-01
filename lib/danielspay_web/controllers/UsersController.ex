b6102c69d5c97a1c3634f4d41299458351f924d1
Carlos Daniel <killer.dk20@gmail.com>
2021-03-01 01:08:06 -0300

    chore:create a controller to insert user

---
+++ lib/danielspay_web/controllers/UsersController.ex
@@ -0,0 +1,24 @@
+defmodule DanielspayWeb.UsersController do
+  use DanielspayWeb, :controller
+
+  alias Danielspay.User
+
+  def create(conn, params) do
+    params
+    |> Danielspay.create_user()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %User{} = user}, conn) do
+    conn
+    |> put_status(:created)
+    |> render("create.json", user: user)
+  end
+
+  defp handle_response({:error, result}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> put_view(DanielspayWeb.ErrorView)
+    |> render("400.json", result: result)
+  end
+end
